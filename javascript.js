25c8456660bb188f71c1cb0feae1fe9ec9eedebc
Steven <steven.guzman@yachaytech.edu.ec>
2022-04-29 05:42:32 -0500

    Completed comments.

--- javascript.js
+++ javascript.js
@@ -1,2 +1,4 @@
 //Create a function which creates the grid by creating n*n number of divs to set the dimension.
 //For this function, use float.
+//Formula for the grid using float is the width or the height of the container divided by n.
+//n represents the dimension number.
